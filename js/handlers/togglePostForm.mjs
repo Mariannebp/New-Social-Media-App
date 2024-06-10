45b6cf266f1ed1636d5e8549dd03f9f38fff0bd7
Marianne Pedersen <mariannebpedersen@gmail.com>
2024-06-10 18:29:29 +0200

    adjusted design on add-new-post button/box

--- js/handlers/togglePostForm.mjs
+++ js/handlers/togglePostForm.mjs
@@ -6,15 +6,22 @@
 export function setTogglePostFormListener() {
   const toggleForm = document.querySelector("#newPost");
   const postForm = document.querySelector("#addNewPost");
-  const postFormColor = document.querySelector("#newPostProfile");
+  const postFormColorMain = document.querySelector("#newPostMain");
+  const postFormColorProfile = document.querySelector("#newPostProfile")
   const postFormHeading = document.querySelector("#newPostH1");
 
+  const path = location.pathname;
+
   toggleForm.addEventListener("click", () => {
-    postForm.classList.toggle("visually-hidden")
-    postFormColor.classList.toggle("btn-buttonmain")
-    postFormColor.classList.toggle("bg-gray")
-    postFormHeading.classList.toggle("text-white")
-    postFormHeading.classList.toggle("text-secondary")
+    postForm.classList.toggle("visually-hidden");
+    if (path === `/index.html`) {
+      postFormColorMain.classList.toggle("bg-buttonmain");
+      postFormColorMain.classList.toggle("bg-gray");
+    } else {
+      postFormColorProfile.classList.toggle("bg-buttonmain");
+      postFormColorProfile.classList.toggle("bg-gray");
+    }
+    postFormHeading.classList.toggle("text-white");
+    postFormHeading.classList.toggle("text-secondary");
   })
-  // toggleForm.addEventListener("click", () => postBg.classList.toggle("btn-buttonmain"));
 }
